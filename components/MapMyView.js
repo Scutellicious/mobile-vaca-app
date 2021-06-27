d1ff462269531950f80bb10e6527fe4f3355a128
Adrienne Scutellaro <78447014+Scutellicious@users.noreply.github.com>
2021-06-27 19:32:23 -0400

    map displays on click

---
+++ components/MapMyView.js
@@ -0,0 +1,83 @@
+import MapView from 'react-native-maps';
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+} from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    ...StyleSheet.absoluteFillObject,
+    height: 400,
+    width: 400,
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  map: {
+    ...StyleSheet.absoluteFillObject,
+  },
+});
+
+export default class MapMyView extends React.Component {
+  render() {
+    return (
+      <MapView
+        style={{flex: 1}}
+        region={{
+          latitude: 42.882004,
+          longitude: 74.582748,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+        showsUserLocation={true}
+      />
+    );
+  }
+}
+
+// const MapMyView = () => {
+//   return (
+//     <View style={styles.container}>
+//       <MapView
+//         style={styles.map}
+//         region={{
+//           latitude: 37.78825,
+//           longitude: -122.4324,
+//           latitudeDelta: 0.015,
+//           longitudeDelta: 0.0121,
+//         }}></MapView>
+//     </View>
+//   );
+// };
+
+// export default MapMyView;
+// import React from 'react';
+// import {View, Text} from 'react-native';
+// import {connect} from 'react-redux';
+// import {setActivities} from '../store/activities';
+
+// export class ActivitiesView extends React.Component {
+//   componentDidMount() {
+//     this.props.getActivities();
+//   }
+
+//   render() {
+//     const activities = this.props.activities || [];
+//     return (
+//       <View className="act">
+//         {activities.map((act, idx) => {
+//           return (
+//             <Text key={idx}>
+//               <Text h3>{act}</Text>
+//             </Text>
+//           );
+//         })}
+//       </View>
+//     );
+//   }
+// }
